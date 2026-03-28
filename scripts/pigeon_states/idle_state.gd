fcb6f8acc6b41e573ca135b0e84b2c3554770bfd
Martin Jakubik <martindzejky@gmail.com>
2026-03-28 14:34:49 +0100

    fix retriggering dialog immediately

--- scripts/pigeon_states/idle_state.gd
+++ scripts/pigeon_states/idle_state.gd
@@ -8,11 +8,15 @@ func exit() -> void:
   pass
 
 
-func update(_delta: float) -> void:
-  if Input.is_action_just_pressed('interact') and Game.current_target_npc:
-    Events.dialogue_started.emit(Game.current_target_npc)
+func _unhandled_input(event: InputEvent) -> void:
+  if not is_current():
     return
+  if event.is_action_pressed('interact') and Game.current_target_npc:
+    get_viewport().set_input_as_handled()
+    Events.dialogue_started.emit(Game.current_target_npc)
 
+
+func update(_delta: float) -> void:
   var direction := Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
   if direction.length() > 0.0:
     state_machine.replace('run')
