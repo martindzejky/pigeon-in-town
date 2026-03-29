e7eb318082f5aeb764fa1ca9d52df7e651fc8be0
Martin Jakubik <martindzejky@gmail.com>
2026-03-29 09:31:04 +0200

    global npm memory

--- autoload/game.gd
+++ autoload/game.gd
@@ -2,6 +2,10 @@ extends Node
 
 # global game state
 
+## global npm memory shared by all NPCs to track cross-conversation flags
+var npc_memory: NpcMemory = NpcMemory.new()
+
+## the npc character the pigeon is currently interacting with
 var current_target_npc: Npc = null:
   set(value):
     if current_target_npc == value:
