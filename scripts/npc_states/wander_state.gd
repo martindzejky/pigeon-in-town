636c9734eca151f1e47fa40fe7d2ed9dde731b3d
Martin Jakubik <martindzejky@gmail.com>
2026-03-29 15:00:00 +0200

    backup timer for wander state

--- scripts/npc_states/wander_state.gd
+++ scripts/npc_states/wander_state.gd
@@ -2,14 +2,20 @@ extends NpcState
 
 @export var wander_radius: float = 100.0
 @export var wander_reached_distance: float = 10.0
+@export var wander_timeout_sec: float = 4.0
 
 var _starting_position: Vector2
 var _wander_target: Vector2
+var _timeout_timer: Timer
 
 
 func _ready() -> void:
-  # store the npc's initial world position so we always wander around it
   _starting_position = npc.global_position
+  _timeout_timer = Timer.new()
+  _timeout_timer.wait_time = wander_timeout_sec
+  _timeout_timer.one_shot = true
+  _timeout_timer.timeout.connect(_on_timeout)
+  add_child(_timeout_timer)
 
 
 func enter() -> void:
@@ -18,12 +24,18 @@ func enter() -> void:
     _starting_position
     + Vector2.UP.rotated(randf_range(0.0, 2.0 * PI)) * randf_range(0.0, wander_radius)
   )
+  _timeout_timer.start()
 
 
 func exit() -> void:
-  pass
+  _timeout_timer.stop()
 
 
 func update(delta: float) -> void:
   if npc.move_to_position(_wander_target, delta, wander_reached_distance):
+    state_machine.pop()
+
+
+func _on_timeout() -> void:
+  if is_current():
     state_machine.pop()
